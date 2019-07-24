3030cba28af4fb1df37d582ec5350a263f0ff82c
Chris Schadl <cschadl@gmail.com>
2019-07-24 17:20:31 -0500

    Ooops, got that backawards...

--- kdtree.h
+++ kdtree.h
@@ -206,7 +206,7 @@ public:
 			value_type const dist_left = node->left_child ? std::abs(p[s] - node->left_child->val[s]) : max_val;
 			value_type const dist_right = node->right_child ? std::abs(p[s] - node->right_child->val[s]) : max_val;
 
-			if (p[s] > node->val[s])
+			if (p[s] < node->val[s])
 			{
 				if (dist_this_node > dist_left || left_bbox.contains(p))
 					node_stack.emplace(query_stack_entry{(node->left_child).get(), left_bbox});
