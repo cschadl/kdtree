3375ca17b3aa92d6b06ba68f68aba0e5078e284c
Chris Schadl <cschadl@gmail.com>
2024-06-02 15:54:44 -0500

    convert point_traits tests to GTest

--- tests/src/test_point_traits.cpp
+++ tests/src/test_point_traits.cpp
@@ -1,28 +1,21 @@
-#include <kdtree/point_traits.hpp>
+#include <gtest/gtest.h>
 
-#include <tut/tut.hpp>
+#include <kdtree/point_traits.hpp>
 
 using namespace cds::kdtree;
 
-namespace tut
+namespace
 {
-	struct test_point_traits_data
-	{
-		using point2d_t = std::array<double, 2>;
-		using point3d_t = std::array<double, 3>;
-	};
+	using point2d_t = std::array<double, 2>;
+	using point3d_t = std::array<double, 3>;
+}
 
-	using test_point_traits_t = test_group<test_point_traits_data, 10>;
-	test_point_traits_t test_point_traits("point traits");
 
-	template <> template <>
-	void test_point_traits_t::object::test<1>()
-	{
-		set_test_name("point_traits::create()");
-
-		point3d_t point = point_traits<point3d_t>::create(12.0);
-		ensure(point[0] == 12.0);
-		ensure(point[1] == 12.0);
-		ensure(point[2] == 12.0);
-	}
-};
+TEST(point_traits, create)
+{
+	point3d_t point = point_traits<point3d_t>::create(12.0);
+	
+	EXPECT_EQ(point[0], 12.0);
+	EXPECT_EQ(point[1], 12.0);
+	EXPECT_EQ(point[2], 12.0);
+}
